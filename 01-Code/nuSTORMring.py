c84d3ed010dfd19b7c3665f47f5edba4f54ca338
OmarIbnaNazim <omar@DESKTOP-7EIHU3I.localdomain>
2021-10-06 22:22:48 +0600

    nuSTORMring: Absorbed particles ignored by setting eventWeight=0

--- 01-Code/nuSTORMring.py
+++ 01-Code/nuSTORMring.py
@@ -24,12 +24,11 @@ piEI = []
 
 runNum = 1
 eventNum = 1
-eventWeight = 1 #Not sure about the values
-pdgCode = 1
-electronmass = 9.11*10**-31 # not sure about the units
+#Not sure about the values
+
 
 #Running PiEvt and NuEvt together
-for i in range(1000):
+for i in range(10000):
 
     piEvt= piEvtInst.PionEventInstance(8)
     piEI.append(piEvt)
@@ -45,47 +44,49 @@ obj.rootStructure()
 
 #Storing the information in particle format
 for (piEvt,nuEvt) in zip(piEI,nuEI):
- #Target
- testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "pi-")
- location = 'target'
- obj.addParticle(location, testParticle)
+  eventWeight = 1 
+  #Target
+  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, "pi-")
+  location = 'target'
+  obj.addParticle(location, testParticle)
  
- #Production Straight 
- testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "pi-")
- location = 'productionStraight'
- obj.addParticle(location, testParticle)
+  #Production Straight 
+  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, "pi-")
+  location = 'productionStraight'
+  obj.addParticle(location, testParticle)
  
- # 1.Pion decay
- tpi = piEvt.gettpi()
- TSCpi = piEvt.getTraceSpaceCoord()
- ppiz = piEvt.getppiGen()
- testParticle = particle.particle(1, eventNum, TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[0], TSCpi[4]*ppiz, TSCpi[5]*ppiz, ppiz, tpi, eventWeight, "pi-")
- location = 'pionDecay'
- obj.addParticle(location, testParticle)
+  # 1.Pion decay
+  tpi = piEvt.gettpi()
+  TSCpi = piEvt.getTraceSpaceCoord()
+  ppiz = piEvt.getppiGen()
+  testParticle = particle.particle(1, eventNum, TSCpi[0], TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[4]*ppiz, TSCpi[5]*ppiz, ppiz, tpi, eventWeight, "pi-")
+  location = 'pionDecay'
+  obj.addParticle(location, testParticle)
 
- #Pion Flash
- numu4mmtm = piEvt.getnumu4mmtm()
- numu3mmtm = numu4mmtm[1]
- testParticle = particle.particle(1, eventNum, TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[0], numu3mmtm[0], numu3mmtm[1], numu3mmtm[2], tpi, eventWeight, "numu")
- location="piFlashNu"
- obj.addParticle(location, testParticle)
+  #Pion Flash
+  numu4mmtm = piEvt.getnumu4mmtm()
+  numu3mmtm = numu4mmtm[1]
+  testParticle = particle.particle(1, eventNum, TSCpi[0], TSCpi[1], TSCpi[2], TSCpi[3], numu3mmtm[0], numu3mmtm[1], numu3mmtm[2], tpi, eventWeight, "numu")
+  location="piFlashNu"
+  obj.addParticle(location, testParticle)
 
    
- #Muon Production
- mu4mmtm = piEvt.getmu4mmtm()
- mu3mmtm = mu4mmtm[1]
- testParticle = particle.particle(1, eventNum, TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[0], mu3mmtm[0], mu3mmtm[1], mu3mmtm[2], tpi, eventWeight, "mu-")
- location = "muonProduction"
- obj.addParticle(location, testParticle)
+  #Muon Production
+  mu4mmtm = piEvt.getmu4mmtm()
+  mu3mmtm = mu4mmtm[1]
+  testParticle = particle.particle(1, eventNum, TSCpi[0], TSCpi[1], TSCpi[2], TSCpi[3], mu3mmtm[0], mu3mmtm[1], mu3mmtm[2], tpi, eventWeight, "mu-")
+  location = "muonProduction"
+  obj.addParticle(location, testParticle)
 
    
- if(nuEvt.getAbsorbed() == False):
+  if(nuEvt.getAbsorbed() == True):
+     eventWeight=0
     
   #1. Muon Decay
   tmu = nuEvt.gettmu()
   TSCmu = nuEvt.getTraceSpaceCoord()
   pmuz = nuEvt.getpmu()
-  testParticle = particle.particle(1, eventNum, TSCmu[1], TSCmu[2], TSCmu[3], TSCmu[0], TSCmu[4]*pmuz, TSCmu[5]*pmuz, pmuz, tmu, eventWeight, "mu-")
+  testParticle = particle.particle(1, eventNum, TSCmu[0], TSCmu[1], TSCmu[2], TSCmu[3], TSCmu[4]*pmuz, TSCmu[5]*pmuz, pmuz, tmu, eventWeight, "mu-")
   location='muonDecay'
   obj.addParticle(location, testParticle)
 
@@ -93,7 +94,7 @@ for (piEvt,nuEvt) in zip(piEI,nuEI):
   #2. Electron Production
   e4mmtm = nuEvt.gete4mmtm()
   e3mmtm = e4mmtm[1]
-  testParticle = particle.particle(1, eventNum, TSCmu[1], TSCmu[2], TSCmu[3], TSCmu[0], e3mmtm[0], e3mmtm[1], e3mmtm[2], tmu, eventWeight, "e-")
+  testParticle = particle.particle(1, eventNum, TSCmu[0], TSCmu[1], TSCmu[2], TSCmu[3], e3mmtm[0], e3mmtm[1], e3mmtm[2], tmu, eventWeight, "e-")
   location='eProduction'
   obj.addParticle(location, testParticle)
 
@@ -101,7 +102,7 @@ for (piEvt,nuEvt) in zip(piEI,nuEI):
   #3. nue Production
   nue4mmtm = nuEvt.getnue4mmtm()
   nue3mmtm = nue4mmtm[1]
-  testParticle = particle.particle(1, eventNum, TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[0], nue3mmtm[0], nue3mmtm[1], nue3mmtm[2], tpi, eventWeight, "nue")
+  testParticle = particle.particle(1, eventNum, TSCmu[0], TSCmu[1], TSCmu[2], TSCmu[3], nue3mmtm[0], nue3mmtm[1], nue3mmtm[2], tmu, eventWeight, "nue")
   location='nueProduction'
   obj.addParticle(location, testParticle)
  
@@ -109,59 +110,22 @@ for (piEvt,nuEvt) in zip(piEI,nuEI):
   #4. numu Production
   numu4mmtm = nuEvt.getnumu4mmtm()
   numu3mmtm = numu4mmtm[1]
-  testParticle = particle.particle(runNum, eventNum, TSCpi[1], TSCpi[2], TSCpi[3], TSCpi[0], numu3mmtm[0], numu3mmtm[1], numu3mmtm[2], tpi, eventWeight, "numu")
+  testParticle = particle.particle(runNum, eventNum, TSCmu[0], TSCmu[1], TSCmu[2], TSCmu[3], numu3mmtm[0], numu3mmtm[1], numu3mmtm[2], tmu, eventWeight, "numu")
   location='numuProduction'
   obj.addParticle(location, testParticle)
   
   # numu Detector
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "numu")
+  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, "numu")
   location = 'numuDetector'
   obj.addParticle(location, testParticle)
   
   # nue Detector
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "nue")
+  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, "nue")
   location = 'nueDetector'
   obj.addParticle(location, testParticle)
   obj.fill()
  
- else:
-  #1. Muon Decay
-
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "mu-")
-  location='muonDecay'
-  obj.addParticle(location, testParticle)
-
    
-  #2. Electron Production
-
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "e-")
-  location='eProduction'
-  obj.addParticle(location, testParticle)
-
-  
-  #3. nue Production
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "nue")
-  location='nueProduction'
-  obj.addParticle(location, testParticle)
- 
-  
-  #4. numu Production
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "numu")
-  location='numuProduction'
-  obj.addParticle(location, testParticle)
-  
-  # numu Detector
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "numu")
-  location = 'numuDetector'
-  obj.addParticle(location, testParticle)
-  
-  # nue Detector
-  testParticle = particle.particle(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "nue")
-  location = 'nueDetector'
-  obj.addParticle(location, testParticle)
-  obj.fill()
-   
-
 obj.write()
 obj.outFileClose()   
 
