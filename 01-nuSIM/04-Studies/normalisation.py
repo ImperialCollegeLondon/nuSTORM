Fixed wrong transformation from nuSTORM global to transfer line local coordinates in PionEventInstance and added changes in calling of RandomGenerator constructor and methods to normalisation.py .

01-nuSIM/04-Studies/normalisation.py
@@ -221,7 +221,7 @@ class normalisation:
       ypd = tsc[5]
       pi.getLifetime()
       td = lifetime*1E9 + t
-      pPion =pi.getppiGen()
+      #pPion =pi.getppiGen()
       #zd = sd - tlCmplxLength
       pPion = pi.getppiGen()
       pxd = pPion*xpd
@@ -412,7 +412,7 @@ if __name__ == "__main__" :
          "\n", trfCmplxFile)
     outFilename = rootFilename#"norm.root"
 # Get machine and run parameters
-    RndmGen = Rndm.RandomGenerator(rootInputFilename)
+    RndmGen = Rndm.RandomGenerator(rootInputFilename,'histP','histXPS')
     nuStrt = nuPrdStrt.nuSTORMPrdStrght(filename)
     psLength = nuStrt.ProdStrghtLen()
     nuTrLnCmplx = nuTrf.nuSTORMTrfLineCmplx(trfCmplxFile)
@@ -432,10 +432,10 @@ if __name__ == "__main__" :
 for event in range(nEvents):
 # set its values
     s = 0.0
-    xl, xpl = RndmGen.getRandom2D('histXPS')
-    yl, ypl = RndmGen.getRandom2D('histYPS')
+    xl, xpl = RndmGen.getRandom2D()
+    yl, ypl = RndmGen.getRandom2D()
     zl = -tlCmplxLength
-    pPion = RndmGen.getRandom('histP')
+    pPion = RndmGen.getRandom()
     pxl = pPion*xpl
     pyl = pPion*ypl
     pzl = np.sqrt(pPion*pPion - pxl**2 - pyl**2)
@@ -476,7 +476,6 @@ for event in range(nEvents):
         normInst.decayPiInPS()
 # decay the muons
         if (muDcyFlag): normInst.decayMuons()
-
 
 
 #  write to the root structure